670dd64f42844610c4ac9c65a31b3963b5519928
fedro <fjodor.makaroff@gmail.com>
2023-05-15 17:16:59 +0300

    add profile

---
+++ ImageFeed/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by paul kellerman on 15.05.2023.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet private var LogoutBtn: UIButton!
+    @IBOutlet private var LoginLabel: UILabel!
+    @IBOutlet private var NameLabel: UILabel!
+    @IBOutlet private var AvatarImage: UIImageView!
+    @IBOutlet private var DescriptionLabel: UILabel!
+    
+    
+    @IBAction func DidTapLogoutButton(_ sender: UIButton) {
+    }
+}
