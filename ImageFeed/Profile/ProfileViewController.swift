499b7e2db9c2c8fef33ba85cf253288cc7d57430
fedro <fjodor.makaroff@gmail.com>
2023-05-19 13:20:50 +0300

    забыл добавить реализацию кода хотя он был и написан

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -1,20 +1,79 @@
-//
-//  ProfileViewController.swift
-//  ImageFeed
-//
-//  Created by paul kellerman on 15.05.2023.
-//
-
 import UIKit
 
 final class ProfileViewController: UIViewController {
     
-    @IBOutlet private var LogoutBtn: UIButton!
-    @IBOutlet private var LoginLabel: UILabel!
-    @IBOutlet private var NameLabel: UILabel!
-    @IBOutlet private var AvatarImage: UIImageView!
-    @IBOutlet private var DescriptionLabel: UILabel!
-    
-    @IBAction func DidTapLogoutButton() {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let ProfileImage = UIImage(named: "avatar")
+        let imageView = UIImageView(image: ProfileImage)
+        
+        imageView.tintColor = .gray
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(imageView)
+        
+        imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 32).isActive = true
+        imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        
+        let nameLabel = UILabel()
+        nameLabel.text = "Екатерина Новикова"
+        nameLabel.font = UIFont.systemFont(ofSize: 23, weight: UIFont.Weight.semibold)
+        nameLabel.textColor = .white
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(nameLabel)
+        NSLayoutConstraint.activate([
+            nameLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            nameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8),
+            nameLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor)
+        ])
+        
+        let loginLabel = UILabel()
+        loginLabel.text = "@ekaterina_nov"
+        loginLabel.textColor = UIColor(named: "LoginName")
+        loginLabel.font = UIFont.systemFont(ofSize: 13)
+        loginLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(loginLabel)
+        
+        NSLayoutConstraint.activate([
+            loginLabel.trailingAnchor.constraint(equalTo: nameLabel.trailingAnchor),
+            loginLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            loginLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor)
+        ])
+        
+        let descriptionLabel = UILabel()
+        descriptionLabel.text = "Hello, World!"
+        descriptionLabel.font = UIFont.systemFont(ofSize: 13)
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(descriptionLabel)
+        
+        NSLayoutConstraint.activate([
+            descriptionLabel.leadingAnchor.constraint(equalTo: loginLabel.leadingAnchor),
+            descriptionLabel.topAnchor.constraint(equalTo: loginLabel.bottomAnchor, constant: 8)
+        ])
+        
+        
+        let logoutBtn = UIButton.systemButton(
+            with: UIImage(named: "logout")!,
+            target: self,
+            action: Selector?.none
+        )
+        
+        logoutBtn.tintColor = UIColor(named: "LogoutButtonColor")
+        logoutBtn.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(logoutBtn)
+        logoutBtn.widthAnchor.constraint(equalToConstant: 24).isActive = true
+        logoutBtn.heightAnchor.constraint(equalToConstant: 24).isActive = true
+        
+        logoutBtn.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
+        logoutBtn.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 55).isActive = true
     }
+    
+    @objc private func didTapButton() {}
 }
